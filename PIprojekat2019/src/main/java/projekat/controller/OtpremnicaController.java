eea3e35bb7bbad180ad623c19dc24c5a21e987f1
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2020-11-24 15:52:04 +0100

    Kontroleri za stavku otpremnice i otpremnicu. Zavrseni svi kontroleri.

--- PIprojekat2019/src/main/java/projekat/controller/OtpremnicaController.java
+++ PIprojekat2019/src/main/java/projekat/controller/OtpremnicaController.java
@@ -2,9 +2,25 @@ package projekat.controller;
 
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import projekat.dto.OtpremnicaDTO;
+import projekat.model.Faktura;
+import projekat.model.Otpremnica;
+import projekat.service.intrfc.FakturaServiceInterface;
+import projekat.service.intrfc.OtpremnicaServiceInterface;
 
 
 
@@ -13,15 +29,131 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/otpremnice")
 public class OtpremnicaController {
 	
-//	@Autowired
-//	private OtpremnicaService otpremnicaService;
-//	
-//	@RequestMapping
-//	public ResponseEntity<List<Otpremnica>> getOtpremnice() {
-//
-//		List<Otpremnica> otpremnice = otpremnicaService.findAll();
-//
-//		return new ResponseEntity<>(otpremnice, HttpStatus.OK);
-//	}
+	@Autowired
+	private OtpremnicaServiceInterface otpremnicaServiceInterface;
+	
+	@Autowired
+	private FakturaServiceInterface fakturaServiceInterface;
+	
+	@GetMapping(path = "/all")
+	public List<Otpremnica> getAll() {
+		return otpremnicaServiceInterface.findAll();
+	}
+	
+	@PostMapping(path = "/pojedinacnaOtpremnica")
+	public ResponseEntity<OtpremnicaDTO> pojedinacnaOtpremnica(@RequestParam("broj_otpremnice") String brojOtpremnice) {
+		
+		int brojOtpremniceInt = Integer.parseInt(brojOtpremnice);
+		
+		Otpremnica otpremnica = otpremnicaServiceInterface.findByBrojOtpremnice(brojOtpremniceInt);
+		
+		if(otpremnica == null) {
+			return new ResponseEntity<OtpremnicaDTO>(HttpStatus.BAD_REQUEST);
+		}else {
+			return new ResponseEntity<OtpremnicaDTO>(new OtpremnicaDTO(otpremnica), HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping(path = "/dodajOtpremnicu")
+	public ResponseEntity<Void> dodajOtpremnicu(@RequestParam("broj_otpremnice") String brojOtpremnice, 
+			@RequestParam("kupac") String kupac, @RequestParam("adresa_isporuke") String adresaIsporuke, 
+			@RequestParam("datum_isporuke") String datumIsporuke, @RequestParam("prevoznik") String prevoznik,
+			@RequestParam("faktura") String brojFakture) throws ParseException {
+		
+		System.out.println("Broj otpremnice: " + brojOtpremnice);
+		System.out.println("Kupaac: " + kupac);
+		System.out.println("Adresa isporuke: " + adresaIsporuke);
+		System.out.println("Datum isporuke: " + datumIsporuke);
+		System.out.println("Prevoznik: " + prevoznik);
+		System.out.println("Faktura: " +  brojFakture);
+		
+		int brojOtpremniceInt = Integer.parseInt(brojOtpremnice);
+		
+		String datum = datumIsporuke;
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = formatter.parse(datum);
+	    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+	    
+	    int brojFaktureInt = Integer.parseInt(brojFakture);
+	    
+	    Faktura faktura = fakturaServiceInterface.findByBrojFakture(brojFaktureInt);
+	    
+	    Otpremnica otpremnica = new Otpremnica();
+	    otpremnica.setBrojOtpremnice(brojOtpremniceInt);
+	    otpremnica.setKupac(kupac);
+	    otpremnica.setAdresaIsporuke(adresaIsporuke);
+	    otpremnica.setDatumIsporuke(sqlDate);
+	    otpremnica.setPrevoznik(prevoznik);
+	    otpremnica.setPotpisVozaca(false);
+	    otpremnica.setPrimioRobu(false);
+	    otpremnica.setStavkeOtpremnice(otpremnica.getStavkeOtpremnice());
+	    otpremnica.setFaktura(faktura);
+	    otpremnicaServiceInterface.save(otpremnica);
+	    
+	    System.out.println("Dodata je nova otpremnica");
+		
+		return new ResponseEntity<Void>(HttpStatus.OK);
+		
+	}
+	
+	@PostMapping(path = "/izmeniOtpremnicu")
+	public ResponseEntity<Void> izmeniOtpremnicu(@RequestParam("broj_otpremnice") String brojOtpremnice, 
+			@RequestParam("novi_broj") String noviBroj,
+			@RequestParam("kupac") String kupac, @RequestParam("adresa_isporuke") String adresaIsporuke, 
+			@RequestParam("datum_isporuke") String datumIsporuke, @RequestParam("prevoznik") String prevoznik,
+			@RequestParam("faktura") String brojFakture) throws ParseException {
+		
+		
+		int brojOtpremniceInt = Integer.parseInt(brojOtpremnice);
+		
+		Otpremnica otpremnica = otpremnicaServiceInterface.findByBrojOtpremnice(brojOtpremniceInt);
+		
+		int noviBrojInt = Integer.parseInt(noviBroj);
+		String datum = datumIsporuke;
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = formatter.parse(datum);
+	    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+	    
+	    int brojFaktureInt = Integer.parseInt(brojFakture);
+	    
+	    Faktura faktura = fakturaServiceInterface.findByBrojFakture(brojFaktureInt);
+		
+		if(otpremnica != null) {
+			otpremnica.setBrojOtpremnice(noviBrojInt);
+			otpremnica.setKupac(kupac);
+			otpremnica.setAdresaIsporuke(adresaIsporuke);
+			otpremnica.setDatumIsporuke(sqlDate);
+			otpremnica.setPrevoznik(prevoznik);
+			otpremnica.setPotpisVozaca(true);
+			otpremnica.setPrimioRobu(true);
+			otpremnica.setFaktura(faktura);
+			otpremnica.setStavkeOtpremnice(otpremnica.getStavkeOtpremnice());
+			
+			System.out.println("Izmenjena je otpremnica.");
+			
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}else {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
+		
+	}
 
+	
+	@PostMapping(path = "/obrisiOtpremnicu")
+	public ResponseEntity<Void> obrisiOtpremnicu(@RequestParam("broj_otpremnice") String brojOtpremnice){
+		
+		int brojOtpremniceInt = Integer.parseInt(brojOtpremnice);
+		
+		Otpremnica otpremnica = otpremnicaServiceInterface.findByBrojOtpremnice(brojOtpremniceInt);
+		
+		if(otpremnica == null) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
+		
+		otpremnicaServiceInterface.remove(otpremnica.getIdOtpremnice());
+		
+		System.out.println("Obrisana je otpremnica.");
+		
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 }
---
+++ PIprojekat2019/src/main/java/projekat/controller/StavkaOtpremniceController.java
@@ -0,0 +1,148 @@
+package projekat.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import projekat.dto.StavkaOtpremniceDTO;
+import projekat.model.Otpremnica;
+import projekat.model.StavkaOtpremnice;
+import projekat.service.intrfc.JedinicaMereServiceInterface;
+import projekat.service.intrfc.OtpremnicaServiceInterface;
+import projekat.service.intrfc.StavkaOtpremniceServiceInterface;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "api/stavkeotpremnice")
+public class StavkaOtpremniceController {
+	
+	@Autowired
+	private StavkaOtpremniceServiceInterface stavkaOtpremniceServicenterface;
+	
+	@Autowired
+	private JedinicaMereServiceInterface jedinicaMereServiceInterface;
+	
+	@Autowired
+	private OtpremnicaServiceInterface otpremnicaServiceInterface;
+	
+	@GetMapping(path = "/all")
+	public List<StavkaOtpremnice> getAll() {
+		return stavkaOtpremniceServicenterface.findAll();
+	}
+	
+	@PostMapping(path = "/pojedinacnaStavkaOtpremnice")
+	public ResponseEntity<StavkaOtpremniceDTO> pojedinacnaStavkaOtpremnice(@RequestParam("redni_broj_proizvoda") String redniBrojProizvoda) {
+
+		int redniBrojProizvodaInt = Integer.parseInt(redniBrojProizvoda);
+		
+		StavkaOtpremnice stavkaOtpremnice = stavkaOtpremniceServicenterface.findByRedniBrojProizvoda(redniBrojProizvodaInt);
+		
+		if(stavkaOtpremnice == null) {
+			return new ResponseEntity<StavkaOtpremniceDTO>(HttpStatus.BAD_REQUEST);
+		}else {
+			return new ResponseEntity<StavkaOtpremniceDTO>(new StavkaOtpremniceDTO(stavkaOtpremnice), HttpStatus.OK);
+		}
+		
+	}
+	
+	@PostMapping(path = "/dodajStavkuOtpremnice")
+	public ResponseEntity<Void> dodajStavkuOtpremnice(@RequestParam("redni_broj_proizvoda") String redniBrojProizvoda,
+			@RequestParam("jedinica_mere") String nazivJediniceMere, @RequestParam("cena") String cena,
+			@RequestParam("kolicina") String kolicina, @RequestParam("napomena") String napomena, @RequestParam("otpremnica") String brojOtpremnice){
+		
+		System.out.println("Redni broj proizvoda: " + redniBrojProizvoda);
+		System.out.println("Jedinica mere: " + nazivJediniceMere);
+		System.out.println("Cena: " + cena);
+		System.out.println("Kolicina: " + kolicina);
+		System.out.println("Napomena: " + napomena);
+		System.out.println("Otpremnica: " + brojOtpremnice);
+		
+		int redniBrojProizvodaInt = Integer.parseInt(redniBrojProizvoda);
+		
+	//	JedinicaMere jedinicaMere = jedinicaMereServiceInterface.findByNazivJediniceMere(nazivJediniceMere);
+		
+		double cenaDouble = Double.parseDouble(cena);
+		double kolicinaDouble = Double.parseDouble(kolicina);
+		
+		int brojOtpremniceInt = Integer.parseInt(brojOtpremnice);
+		
+		Otpremnica otpremnica = otpremnicaServiceInterface.findByBrojOtpremnice(brojOtpremniceInt);
+		
+		StavkaOtpremnice stavkaOtpremnice = new StavkaOtpremnice();
+		stavkaOtpremnice.setRedniBrojProizvoda(redniBrojProizvodaInt);
+		stavkaOtpremnice.setJedinicaMere(nazivJediniceMere);
+		stavkaOtpremnice.setCena(cenaDouble);
+		stavkaOtpremnice.setKolicina(kolicinaDouble);
+		stavkaOtpremnice.setNapomena(napomena);
+		stavkaOtpremnice.setOtpremnica(otpremnica);
+		stavkaOtpremniceServicenterface.save(stavkaOtpremnice);
+		
+		System.out.println("Dodata je stavka otpremnice.");
+		
+		return new ResponseEntity<Void>(HttpStatus.OK);
+		
+	}
+	
+	@PostMapping(path =  "/izmeniStavkuOptremnice")
+	public ResponseEntity<Void> izmeniStavkuOtpremnice(@RequestParam("redni_broj_proizvoda") String redniBrojProizvoda,
+			@RequestParam("novi_broj") String noviBroj,
+			@RequestParam("jedinica_mere") String nazivJediniceMere, @RequestParam("cena") String cena,
+			@RequestParam("kolicina") String kolicina, @RequestParam("napomena") String napomena, @RequestParam("otpremnica") String brojOtpremnice) {
+			
+		int redniBrojProizvodaInt = Integer.parseInt(redniBrojProizvoda);
+		
+		StavkaOtpremnice stavkaOtpremnice = stavkaOtpremniceServicenterface.findByRedniBrojProizvoda(redniBrojProizvodaInt);
+		
+		int noviBrojInt = Integer.parseInt(noviBroj);
+		
+		double cenaDouble = Double.parseDouble(cena);
+		double kolicinaDouble = Double.parseDouble(kolicina);
+		
+		int brojOtpremniceInt = Integer.parseInt(brojOtpremnice);
+		
+		Otpremnica otpremnica = otpremnicaServiceInterface.findByBrojOtpremnice(brojOtpremniceInt);
+		
+		if(stavkaOtpremnice != null) {
+			stavkaOtpremnice.setRedniBrojProizvoda(noviBrojInt);
+			stavkaOtpremnice.setJedinicaMere(nazivJediniceMere);
+			stavkaOtpremnice.setCena(cenaDouble);
+			stavkaOtpremnice.setKolicina(kolicinaDouble);
+			stavkaOtpremnice.setNapomena(napomena);
+			stavkaOtpremnice.setOtpremnica(otpremnica);
+			stavkaOtpremniceServicenterface.save(stavkaOtpremnice);
+			
+			System.out.println("Izmenjena je stavka otpremnice.");
+			
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}else {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
+		
+	}
+	
+	@PostMapping(path = "/obrisiStavkuOtpremnice")
+	public ResponseEntity<Void> obrisiStavkuOtpremnice(@RequestParam("redni_broj_proizvoda") String redniBrojProizvoda) {
+		
+		int redniBrojProizvodaInt = Integer.parseInt(redniBrojProizvoda);
+		
+		StavkaOtpremnice stavkaOtpremnice = stavkaOtpremniceServicenterface.findByRedniBrojProizvoda(redniBrojProizvodaInt);
+		
+		if(stavkaOtpremnice == null) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
+		
+		stavkaOtpremniceServicenterface.remove(stavkaOtpremnice.getIdStavkeOtpremnice());
+		
+		System.out.println("Obrisana je stavka otpremnice");
+		
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+}
